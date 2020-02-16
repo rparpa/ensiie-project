9890d5b378fbba9a83ee39e8dd5607d168113928
Mehdi <mehdabdallaoui@gmail.com>
2020-02-16 21:04:41 +0100

    Database v1

--- data/db.sql
+++ data/db.sql
@@ -1,3 +1,4 @@
+/*
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY ,
     firstname VARCHAR NOT NULL ,
@@ -17,3 +18,58 @@ INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
 INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+*/
+
+
+CREATE TABLE Utilisateur(
+    id_user SERIAL NOT NULL PRIMARY KEY,
+    nom VARCHAR(200),
+    prenom VARCHAR(200),
+    pseudo VARCHAR(200),
+    password TEXT,
+    email TEXT,
+    telephone VARCHAR(20),
+    promo VARCHAR(20),
+    droit_publication BOOLEAN NOT NULL,
+    nb_signal_user INTEGER,
+    admin BOOLEAN NOT NULL
+);
+
+CREATE TABLE Etat(
+    id_etat SERIAL NOT NULL PRIMARY KEY,
+    etat VARCHAR(200)
+);
+
+CREATE TABLE Annonce (
+    id_annonce SERIAL NOT NULL PRIMARY KEY,
+    id_user INTEGER NOT NULL,
+    id_etat INTEGER NOT NULL,
+    titre VARCHAR(1024),
+    description TEXT,
+    prix DECIMAL(4, 2),
+    vendu BOOLEAN NOT NULL,
+    nb_signal INTEGER,
+    date_publication TIMESTAMP,
+    CONSTRAINT fk1_user_annonce FOREIGN KEY(id_user) REFERENCES Utilisateur(id_user),
+    CONSTRAINT fk2_etat_annonce FOREIGN KEY(id_etat) REFERENCES Etat(id_etat)
+);
+
+CREATE TABLE Categorie (
+    id_categorie SERIAL NOT NULL PRIMARY KEY,
+    categorie VARCHAR(200)
+);
+
+CREATE TABLE Categorie_annonce (
+    id_annonce INTEGER NOT NULL,
+    id_categorie INTEGER NOT NULL,
+    CONSTRAINT pk_categorie_annonce PRIMARY KEY(id_annonce,id_categorie),
+    CONSTRAINT fk1_categorie_annonce FOREIGN KEY(id_annonce) REFERENCES Annonce(id_annonce),
+    CONSTRAINT fk2_categorie_annonce FOREIGN KEY(id_categorie) REFERENCES Categorie(id_categorie)
+);
+
+CREATE TABLE Image (
+    id_image SERIAL NOT NULL PRIMARY KEY,
+    id_annonce INTEGER NOT NULL,
+    url TEXT,
+    CONSTRAINT fk_annonce_image FOREIGN KEY (id_annonce) REFERENCES Annonce(id_annonce)
+);
