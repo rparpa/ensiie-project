ab486a60f82d128040f9bc7a113cb4466fb3fe35
Corentin Simon <corentin@MacBook-Pro-de-Corentin.local>
2020-02-18 11:08:31 +0100

    encore un peu d'HTML

--- public/index.php
+++ public/index.php
@@ -12,9 +12,12 @@ $users = $userRepository->fetchAll();
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" >
     <a class="navbar-brand" href="#">In Fact</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -27,23 +30,50 @@ $users = $userRepository->fetchAll();
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Connexion</button>
+            <button class="btn btn-outline-light my-2 my-sm-0" type="button" data-toggle="modal" data-target="#exampleModal" >Connexion</button>
+        </form>
+
+        <form class="form-inline my-2 my-lg-0 pl-2">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="button" data-toggle="modal" data-target="#exampleModal" >Inscription</button>
         </form>
     </div>
+
+    <!-- Modal connexion -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
 </nav>
 
 <body>
 
+    <!-- Recherche -->
     <form class="center">
         <div class="form-group">
             <label for="exampleInputJobs">Quoi ?</label>
             <input type="text" class="form-control" id="exampleInputJobs" aria-describedby="jobsHelp" placeholder="">
-            <small id="jobsHelp" class="form-text text-muted">Que cherchez vous ?</small>
+            <small id="jobsHelp" class="form-text text-muted">Quel métier ?</small>
         </div>
         <div class="form-group">
             <label for="exampleInputLocation">Ou ?</label>
             <input type="text" class="form-control" id="exampleInputLocation" placeholder="">
-            <small id="jobsHelp" class="form-text text-muted">Ou ça ? Quelle ville ?</small>
+            <small id="jobsHelp" class="form-text text-muted">Quelle ville ?</small>
         </div>
 
         <button type="submit" class="btn btn-primary">Chercher</button>
@@ -52,9 +82,3 @@ $users = $userRepository->fetchAll();
 </body>
 
 </html>
-
-<script>
-    function myFunction() {
-        document.getElementById("demo").style.color = "red";
-    }
-</script>
