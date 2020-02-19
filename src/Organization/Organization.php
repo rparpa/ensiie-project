eeba2e522e32340d6c88965713f9d83de7d7e182
Pierre Danel <pierre.g.danel@gmail.com>
2020-02-19 10:10:26 +0100

    Ajout classe Organization

--- src/Organization/Organization.php
+++ src/Organization/Organization.php
@@ -7,4 +7,62 @@ namespace Organization;
 class Organization
 {
 
+    private int $id;
+    private string $name;
+    private DateTimeInterface $creationdate;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Organization;
+     */
+    public function setId(int $id): Organization
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int $name
+     * @return Organization;
+     */
+    public function setName(string $name): Organization
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getCreationdate(): DateTimeInterface
+    {
+        return $this->creationdate;
+    }
+
+    /**
+     * @param DateTimeInterface $creationdate
+     * @return Organization
+     */
+    public function setCreationdate(DateTimeInterface $creationdate): Organization
+    {
+        $this->creationdate = $creationdate;
+        return $this;
+    }
+
 }
