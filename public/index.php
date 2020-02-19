91cf10bcae3bf7bf4a79c5574c4a7c8c238e7395
escobar2018 <antoine.escobar@ensiie.fr>
2020-02-19 14:00:38 +0100

    front_userhome_v1

--- public/index.php
+++ public/index.php
@@ -3,18 +3,27 @@
 use Db\Connection;
 use User\UserHydrator;
 use User\UserRepository;
+use Service\AuthenticatorService;
 
 require_once '../src/Bootstrap.php';
+
 
 $userHydrator = new UserHydrator();
 $userRepository = new UserRepository(Connection::get(), $userHydrator);
 $users = $userRepository->fetchAll();
+$authentificatorService = new AuthenticatorService($userRepository);
 
 $data = [
     'users' => $users
 ];
 
+$view = "home";
+
+if ($authentificatorService->isAuthenticated()) {
+    $view = "userhome";
+}
+
 include_once '../src/View/template.php';
-loadView('home', $data);
+loadView($view, $data);
 
 
