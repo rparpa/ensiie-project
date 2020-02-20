132e11798c1be2b1258bf2caaf7bf6114b70f9a3
JonahR <jonathan.valenca@ensiie.fr>
2020-02-20 10:47:05 +0100

    docker : acces a la page index via localhost

--- controller.js
+++ controller.js
@@ -1,17 +1,32 @@
 const http = require('http');
 const url = require('url');
+var fs = require('fs');
 
 module.exports = http.createServer((req, res) => {
 
     // var service = require('./service.js');
     const reqUrl = url.parse(req.url, true);
+    var pathname = url.parse(req.url).pathname;
+    
+    if(pathname == "/accueil") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile('./public/index.html',function (err, data){
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+            res.write(data);
+            res.end();
+        });
+    }
+    else {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(req.method));
+    }
 
     // GET Endpoint
-    if (reqUrl.pathname == '/sample' && req.method === 'GET') {
-        console.log('Request Type:' +
-            req.method + ' Endpoint: ' +
-            reqUrl.pathname);
+    // if (reqUrl.pathname == '/sample' && req.method === 'GET') {
+    //     console.log('Request Type:' +
+    //         req.method + ' Endpoint: ' +
+    //         reqUrl.pathname);
 
-        // service.sampleRequest(req, res);
-    } 
+    //     // service.sampleRequest(req, res);
+    // } 
 });
