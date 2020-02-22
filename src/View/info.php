b0d73fde48d231d1036fc07baa3c72aa50c77d19
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-22 22:33:54 +0100

    Ajout du fil d'actualité

---
+++ src/View/info.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<div class="info">
+    <table class="table" id="infos">
+        <?php include_once 'chat/refresh_info.php' ?>
+    </table>
+</div>
+
+
+
+<script>
+    function autoRefresh_div()
+    {
+        $("#infos").load('refresh_info.php');
+    }
+
+    setInterval('autoRefresh_div()', 1000); // refresh tab after 5 secs
+</script>
