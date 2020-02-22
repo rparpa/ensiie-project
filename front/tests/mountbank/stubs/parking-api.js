cd01d0c9d0db21367df03d3ea3555bb17a6d4f95
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2020-02-22 20:41:54 +0100

    Changed stub header in order to allow CORS

--- front/tests/mountbank/stubs/parking-api.js
+++ front/tests/mountbank/stubs/parking-api.js
@@ -109,6 +109,7 @@ function addService() {
                     is: {
                         statusCode: 200,
                         headers: {
+                            "Access-Control-Allow-Origin": "*",
                             "Content-Type": "application/json"
                         },
                         body: JSON.stringify(response)
