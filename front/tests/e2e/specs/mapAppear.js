93bdddaffd5d5bdadddadf41c61eb25e4c452ab0
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2020-02-23 17:17:19 +0100

    still Change cypress test

--- front/tests/e2e/specs/mapAppear.js
+++ front/tests/e2e/specs/mapAppear.js
@@ -3,7 +3,7 @@ describe('Map and markers appears visible', () => {
       cy.visit('/')
       cy.get('.mgl-map-wrapper').should('be.visible')
       cy.get('.mapboxgl-canvas').should('be.visible')
-      cy.get('.mapboxgl-markerw').should('be.visible')
+      cy.get('.mapboxgl-marker').should('be.visible')
     })
   })
   
