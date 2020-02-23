ebf48ba84855d52e484f1e5cae603fd8cd089140
François SAINTIER <f.saintier@gmail.com>
2020-02-23 22:51:50 +0100

    Add a search address bar with autocompletion based on vue-places.js

--- front/src/components/MapSettings.vue
+++ front/src/components/MapSettings.vue
@@ -1,3 +1,29 @@
 <template>
-    <p>MAP SETTINGS</p>
+  <!-- Champ de recherche -->
+  <places
+    v-model="form.country.label"
+    placeholder="Où allez-vous ?"
+    @change="val => { form.country.data = val }"
+    :options="{ countries: ['FR'], language: 'fr' }">
+  </places>
 </template>
+
+<script>
+  import Places from 'vue-places'
+
+  export default {
+    data() {
+      return {
+        form: {
+          country: {
+            label: null,
+            data: {},
+          },
+        },
+      };
+    },
+    components: {
+      Places
+    }
+  }
+</script>
