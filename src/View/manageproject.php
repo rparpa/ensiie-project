145c9aa574515414704a1f5ae6be1c7cd1e69777
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-23 10:53:16 +0100

    Mise en forme et corrections

--- src/View/manageproject.php
+++ src/View/manageproject.php
@@ -1,6 +1,7 @@
 <?php
 
 use Db\Connection;
+use Organization\Organization;
 use Organization\OrganizationHydrator;
 use Organization\OrganizationRepository;
 use Project\Project;
@@ -18,65 +19,68 @@ $hide = true;
 
 if (isset($data['nameAlreadyExist']) || isset($data['nameEmpty']))
     $hide = false;
-
 ?>
 
-<label for="choix_project">Liste des projets : </label>
-<input list="projects" type="text" id="choix_project" name="choix_project">
-    <datalist id="projects">
-    <?php /** @var Project $project */
-    foreach ($projects as $project) { ?>
-    <option value="<?php echo $project->getName() ?>"
-            data-value="<?php echo $project->getId()  ?>"
-            data-idorga="<?php echo $project->getIdorganization() ?>">
-        <?php } ?>
-</datalist>
-<button id="add_project" onclick="showform()">Ajouter un projet</button>
 
-
-<form novalidate
-      method="post"
-      onsubmit="return verif()"
-      id="formulaire"
-      action="addorupdateproject.php"
-        style="display: <?php echo $hide?'none':'block'?>">
-    <div class="formulaire">
-        <input type="hidden" value="" name="id" id="id">
-        <div align="center">
-            <legend>Projet </legend>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <label for="choix_project">Liste des organisations : </label>
+            <select id="select-projets">
+                <option></option>
+                <?php /** @var Project $project */
+                foreach ($projects as $project) { ?>
+                    <option value="<?php echo $project->getName() ?>"
+                            data-value=<?php echo $project->getId() ?>
+                            data-idorga=<? echo $project->getIdorganization()?>
+                            data-nameorga="<? echo $orgarepository->findOneById($project->getIdorganization())->getName()?>" > <?php echo $project->getName() ?></option>
+                <?php } ?>
+            </select>
         </div>
-        <div align="center">
-            <label class="label-lenght-fix" for="name">Name : <em>*</em></label>
-            <input type="text"
-                   value=""
-                   name="name"
-                   id="name"
-                   required=""><br>
-            <span class="error" aria-live="polite" id="errorname"></span>
-            <?php if (isset($data['nameAlreadyExist'])): ?>
-                <span class="error-message" ><?= $data['nameAlreadyExist'] ?></span>
-            <?php endif; ?>
-            <?php if (isset($data['nameEmpty'])): ?>
-                <span class="error-message"><?= $data['nameEmpty'] ?></span>
-            <?php endif; ?>
-        </div>
-        <div align="center">
-            <input type="hidden" id="idorganization" name="idorganization" value="">
-            <label class="label-lenght-fix" for="idorganisations">Organisation : <em>*</em></label>
-            <input list="organisations" type="text" id="choix_organisations" name="choix_organisations" value="">
-            <datalist id="organisations">
-                <?php /** @var Organization $organization */
-                foreach ($organizations as $organization) { ?>
-                <option value="<?php echo $organization->getName() ?>"
-                        data-value="<?php echo $organization->getId() ?>">
-                    <?php } ?>
-            </datalist>
-        </div>
-        <div align="center">
-            <p><button>Envoyer</button>
+        <div class="col">
+            <button id="add_project" onclick="showform()">Ajouter un projet</button>
         </div>
     </div>
-</form>
+    <div class="row">
+        <form class="formulaire" method="post" onsubmit="return verif()" id="formulaire" action="addorupdateproject.php" style="display: <?php echo $hide?'none':'block'?>">
+            <input type="hidden" value="" name="id" id="id">
+            <div class="container-fluid">
+                <div class="form-row" align="center">
+                    <legend>Projet </legend>
+                </div>
+                <div class="form-row">
+                    <label class="label-lenght-fix" for="name">Name : <em>*</em></label>
+                    <input type="text" value="" name="name" id="name" required="">
+                    <span class="error" aria-live="polite" id="errorname"></span>
+                </div>
+                <div class="form-row">
+                    <?php if (isset($data['nameAlreadyExist'])): ?>
+                        <span class="error-message" ><?= $data['nameAlreadyExist'] ?></span>
+                    <?php endif; ?>
+                </div>
+                <div class="form-row">
+                    <?php if (isset($data['nameEmpty'])): ?>
+                        <span class="error-message"><?= $data['nameEmpty'] ?></span>
+                    <?php endif; ?>
+                </div>
+                <div class="form-row">
+                    <input type="hidden" id="idorganization" name="idorganization" value="">
+                    <label class="label-lenght-fix" for="idorganisations">Organisation : <em>*</em></label>
+                    <select id="select-organizations">
+                        <option></option>
+                        <?php /** @var Organization $organization */
+                        foreach ($organizations as $organization) { ?>
+                            <option value="<?php echo $organization->getName() ?>" data-value=<?php echo $organization->getId() ?>><?php echo $organization->getName() ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="form-row">
+                    <button>Envoyer</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 
 
 <script>
@@ -98,31 +102,43 @@ if (isset($data['nameAlreadyExist']) || isset($data['nameEmpty']))
         document.getElementById('id').value = "";
         document.getElementById('name').value = "";
         document.getElementById('idorganization').value = "";
-        document.getElementById('choix_organisations').value = "";
-
+        document.getElementById('select-organizations').value = "";
         document.getElementById('formulaire').style.display = "block";
     };
 
-    document.getElementById('choix_project').addEventListener('input', function() {
-        document.getElementById('name').value = this.value;
-        document.getElementById('formulaire').style.display = "block";
-        var index = [... document.getElementById('projects').options] //*1
-            .map(o => o.value) //*2
-            .indexOf(this.value) //*3
-        document.getElementById('id').value = document.getElementById('projects').options[index].attributes["data-value"].value;
-        document.getElementById('idorganization').value = document.getElementById('projects').options[index].attributes["data-idorga"].value;
 
-        index = [... document.getElementById('organisations').options] //*1
-            .map(o => o.attributes[1].value) //*2
-            .indexOf(document.getElementById('idorganization').value) //*3
-        document.getElementById('choix_organisations').value = document.getElementById('organisations').options[index].value;
-    });
+    var select = document.getElementById('select-projets');
+    select.addEventListener('change', function () {
+        var index = select.selectedIndex;
+        if (index<1)
+        {
+            document.getElementById('id').value = '';
+            document.getElementById('name').value = '';
+            document.getElementById('select-organizations').value = "";
+            document.getElementById('idorganization').value = '';
+            document.getElementById('formulaire').style.display = "none";
+        }
+        else {
+            document.getElementById('id').value = select.options[index].attributes["data-value"].value;
+            document.getElementById('idorganization').value = select.options[index].attributes["data-idorga"].value;
+            document.getElementById('name').value = this.value;
+            document.getElementById('select-organizations').value = select.options[index].attributes["data-nameorga"].value;
+            document.getElementById('formulaire').style.display = "block";
+        }
+    })
 
-    document.getElementById('choix_organisations').addEventListener('input', function() {
-        const index = [... document.getElementById('organisations').options] //*1
-            .map(o => o.value) //*2
-            .indexOf(this.value) //*3
-        document.getElementById('idorganization').value = document.getElementById('organisations').options[index].attributes["data-value"].value;
-    });
+    var select_orga = document.getElementById('select-organizations');
+    select_orga.addEventListener('change', function () {
+        var index = select_orga.selectedIndex;
+        if (index<1)
+        {
+            document.getElementById('idorganization').value = '';
+        }
+        else
+        {
+            document.getElementById('idorganization').value = select_orga.options[index].attributes["data-value"].value;
+        }
+    })
+
 
 </script>
