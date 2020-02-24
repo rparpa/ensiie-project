e345c15b29ef8f703ba66f38db27bebe94d6634e
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-24 19:18:57 +0100

    Ajout d'un bouton en lieu et place du lien

--- src/View/chefproject.php
+++ src/View/chefproject.php
@@ -50,7 +50,7 @@ $userRepository = new UserRepository(Connection::get(), new UserHydrator());
                 <button name="button-reunions" id="<? echo $project->getId()?>">Réunions</button>
             </div>
             <div class="col">
-                <a class="link-header-item-a" href="project.php?idproject=<? echo $project->getId()?>">Gerer</a>
+                <button name="button-gerer" id="<? echo $project->getId()?>" onclick="location.href='project.php?idproject=<? echo $project->getId()?>'">Gerer</button>
             </div>
         </div>
         <div class="row" id="listusers-<? echo $project->getId()?>" style="display: none;" >
