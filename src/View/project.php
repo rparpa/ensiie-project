2745dc1a26d13f70e20a6fb90ee4834cace82bca
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-24 23:34:08 +0100

    du script et du modal bootstrap

--- src/View/project.php
+++ src/View/project.php
@@ -1,10 +1,12 @@
 <?php
 use Db\Connection;
-use Meeting\Meeting;
 use Meeting\MeetingHydrator;
 use Meeting\MeetingRepository;
+use Organization\OrganizationHydrator;
+use Organization\OrganizationRepository;
 use Project\ProjectHydrator;
 use Project\ProjectRepository;
+use Service\AuthenticatorService;
 use Task\TaskHydrator;
 use Task\TaskRepository;
 use User\User;
@@ -23,6 +25,12 @@ $meetingrepository =
     new MeetingRepository(Connection::get(), new MeetingHydrator());
 $userrepository =
     new UserRepository(Connection::get(), new UserHydrator());
+
+$orgarepository =
+    new OrganizationRepository(Connection::get(),new OrganizationHydrator());
+
+$authenticatorService =
+    new AuthenticatorService($userrepository);
 
 
 $idproject =  !empty($data['idproject']) ? $data['idproject'] : null;
@@ -96,13 +104,76 @@ $idproject =  !empty($data['idproject']) ? $data['idproject'] : null;
     </div>
 </div>
 
+
+<!-- Modal HTML -->
+<div id="Modaltask" class="modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form>
+            <div class="modal-header">
+                <h5 class="modal-title">Ajout d'un tache</h5>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="form-row">
+                    <div class="col">
+                        <label >Title</label>
+                    </div>
+                    <div class="col">
+                        <input id="modal-task-title">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col">
+                        <label >Content</label>
+                    </div>
+                    <div class="col">
+                        <textarea id="modal-task-content"></textarea>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col">
+                        <label >State</label>
+                    </div>
+                    <div class="col">
+                        <input id="modal-task-state" value="Assignée">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col">
+                        <label >Responsable</label>
+                    </div>
+                    <div class="col">
+                        <select id="modal-task-assignee">
+                            <option></option>
+                            <?php
+                            //TODO corriger avec un vrai idorga
+                            $useroforga = $orgarepository->fetchByUser($authenticatorService->getCurrentUserId());
+                            $usersoforga = $userrepository->fetchByOrganization(((object)$useroforga)->organization->getId());
+                            foreach ($usersoforga as $useroforga) {
+                            /** @var User $user */
+                            $user = ((Object)$useroforga)->user;?>
+                            <option data-id="<? echo $user->getId()?>"><?php echo $user->getSurname() . ' ' . $user->getName() ?></option>
+                            <? }?>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal">Cancel</button>
+                <button type="submit" >Save changes</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <script>
 
     document.getElementById("button-user-add").addEventListener('click', function () {
-        var select = document.getElementById("select-user-add");
-        var index = select.selectedIndex;
+        var index = $("#select-user-add").prop("selectedIndex");
         if(index>=0){
-            var iduser = select.options[index].attributes["data-id"].value;
+            var iduser = $("#select-user-add").find(':selected').attr('data-id');
             //TODO ajouter une saisie lors de la selection
             var role = "Larbin";
             $.post({
@@ -118,7 +189,7 @@ $idproject =  !empty($data['idproject']) ? $data['idproject'] : null;
     });
 
     document.getElementById("button-task-add").addEventListener('click', function () {
-        alert('TODO');
+        $("#Modaltask").modal("show");
     });
 
     document.getElementById("button-meeting-add").addEventListener('click', function () {
@@ -166,4 +237,34 @@ $idproject =  !empty($data['idproject']) ? $data['idproject'] : null;
         var $data = {idproject:<?echo $idproject;?>};
         $('#field-meeting-delete').load('field_meetingsofproject.php',$data);
     }
+
+    function submit_modal_task() {
+        var title = $("#modal-task-title").val();
+        var content = $("#modal-task-content").val();
+        var state = $("#modal-task-state").val();
+        var idassignee = $("#modal-task-assignee").find(':selected').attr('data-id');
+        $.post({
+            type:"GET",
+            url:"addtask.php",
+            data:{
+                title:title,
+                content:content,
+                state:state,
+                idcreator:<? echo $authenticatorService->getCurrentUserId();?>,
+                idassignee:idassignee,
+                idproject:<? echo $idproject;?>
+            },success:refreshtask
+        }).done(function () {
+            $('#Modaltask').modal('hide')
+        })
+    }
+
+    $(function(){
+        $('form').submit(function(event){
+            // cancels the form submission
+            event.preventDefault();
+            submit_modal_task();
+        })
+    })
+
 </script>
