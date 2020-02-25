9fed3a9b5c51defbbf8075c2915dcd8d7fda0c34
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2020-02-25 23:06:17 +0100

    Changed card to modal

--- front/src/components/UserLog.vue
+++ front/src/components/UserLog.vue
@@ -17,41 +17,43 @@
         v-model="logIn.password"
         placeholder="Password"
       ></b-input>
-
       <b-button class="mr-2" type="submit" variant="primary">Log-in</b-button>
-      <b-button class="mr-2" variant="secondary" @click="$bvToast.show('signin-toast')">Sign-in</b-button>
-
-      <b-toast id="signin-toast" title="Registration" no-auto-hide>
-        <b-form @submit="onSignIn">
-          <label class="sr-only" for="signin-form-username">Username</label>
-          <b-input
-            id="signin-form-username"
-            class="mb-2"
-            v-model="signIn.username"
-            placeholder="Username"
-          ></b-input>
-
-          <label class="sr-only" for="signin-form-email">Email</label>
-          <b-input-group class="mb-2" prepend="@">
+      <div>
+        <!-- Using modifiers -->
+        <b-button v-b-modal.signInModal>Sign In</b-button>
+        <!-- The modal -->
+        <b-modal id="signInModal" title="Sign In">
+            <b-form @submit="onSignIn">
+            <label class="sr-only" for="signin-form-username">Username</label>
             <b-input
-              id="signin-form-email"
-              type="email"
-              v-model="signIn.email"
-              placeholder="Email"
+              id="signin-form-username"
+              class="mb-2"
+              v-model="signIn.username"
+              placeholder="Username"
             ></b-input>
-          </b-input-group>
 
-          <label class="sr-only" for="signin-form-password">Password</label>
-          <b-input
-            id="signin-form-password"
-            class="mb-3"
-            type="password"
-            v-model="signIn.password"
-            placeholder="Password"
-          ></b-input>
-          <b-button type="submit" variant="primary">Create Account</b-button>
-        </b-form>
-      </b-toast>
+            <label class="sr-only" for="signin-form-email">Email</label>
+            <b-input-group class="mb-2" prepend="@">
+              <b-input
+                id="signin-form-email"
+                type="email"
+                v-model="signIn.email"
+                placeholder="Email"
+              ></b-input>
+            </b-input-group>
+
+            <label class="sr-only" for="signin-form-password">Password</label>
+            <b-input
+              id="signin-form-password"
+              class="mb-3"
+              type="password"
+              v-model="signIn.password"
+              placeholder="Password"
+            ></b-input>           
+            <b-button type="submit" variant="primary" >Create Account</b-button>
+          </b-form>
+        </b-modal>
+      </div>
     </b-form>
   </div>
 </template>
@@ -79,7 +81,7 @@ export default {
     },
     onSignIn(evt) {
       evt.preventDefault()
-
+      
       // JSON.stringify(this.signIn) => send to back end
     }
   }
