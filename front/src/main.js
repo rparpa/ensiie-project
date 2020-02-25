ba63587b2aca0f74baf1be354a5fe0d090d5d376
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2020-02-25 01:01:45 +0100

    Add axios to project
    Axios is a http client library for vue.

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -8,6 +10,7 @@ import App from './App.vue'
 import router from './router'
 
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, Axios)
 
 Vue.config.productionTip = false
 
