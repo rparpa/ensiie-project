3a5207bb54092f9727a2909e2a62596635d1e8f7
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2020-02-26 14:31:05 +0100

    Update database architecture
    All parking spots won't be saved in database anymore.
    Now only reserved parking spots will be written in db.

--- back/src/data/db.sql
+++ back/src/data/db.sql
@@ -15,16 +15,6 @@ CREATE TABLE "Vehicule" (
 	CONSTRAINT "Vehicule_pk" PRIMARY KEY ("id")
 );
 
-CREATE TABLE "ParkingSpot" (
-	"id" serial NOT NULL,
-	"latitude" numeric NOT NULL,
-	"longitude" numeric NOT NULL,
-	"emplacement_id" serial NOT NULL,
-	"pricing" DECIMAL(4,2) NOT NULL,
-	"type" int NOT NULL,
-	CONSTRAINT "ParkingSpot_pk" PRIMARY KEY ("id")
-);
-
 CREATE TABLE "Address" (
 	"id" serial NOT NULL,
 	"city" varchar(100) NOT NULL,
@@ -34,9 +24,15 @@ CREATE TABLE "Address" (
 	CONSTRAINT "Address_pk" PRIMARY KEY ("id")
 );
 
+CREATE TABLE "ReservedParkingSpots" (
+	"record_id" char(40) NOT NULL,
+	CONSTRAINT "ReservedParkingSpots_pk" PRIMARY KEY ("record_id")
+);
+
+
+
 ALTER TABLE "User" ADD CONSTRAINT "User_fk0" FOREIGN KEY ("address_id") REFERENCES "Address"("id");
 ALTER TABLE "Vehicule" ADD CONSTRAINT "Vehicule_fk0" FOREIGN KEY ("owner_id") REFERENCES "User"("id");
-ALTER TABLE "ParkingSpot" ADD CONSTRAINT "ParkingSpot_fk0" FOREIGN KEY ("emplacement_id") REFERENCES "Address"("id");
 
 INSERT INTO "Address"(city, street_name, street_number, type) 
     VALUES ('paris', 'chaussée d''antin', 8, 1);
@@ -45,3 +41,7 @@ INSERT INTO "Address"(city, street_name, street_number, type)
 
 INSERT INTO "User"(username, password, email, address_id) VALUES ('JohnDoe', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'johndoe@gmail.com', 1);
 INSERT INTO "User"(username, password, email, address_id) VALUES ('JaneDoe', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'janedoe@gmail.com', 2);
+
+
+
+
