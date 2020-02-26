5bbbc22a7819d53e826c6950838f754cb84e2294
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-26 09:13:18 +0100

    fix : un peu de tout

--- src/View/info.php
+++ src/View/info.php
@@ -3,14 +3,3 @@
         <?php include_once 'refresh_info.php' ?>
     </table>
 </div>
-
-
-
-<script>
-    function autoRefresh_info()
-    {
-        $("#infos_info").load('refresh_info.php');
-    }
-
-    setInterval('autoRefresh_info()', 5000); // refresh tab after 5 secs
-</script>
