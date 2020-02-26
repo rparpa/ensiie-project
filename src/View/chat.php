a9c7818fca4b27aaf9838f5efb9afbd3207bd2d6
Olivier GATISSOU <oli.gatissou@gmail.com>
2020-02-26 09:43:42 +0100

    fix : parametres + add JS pour le post

--- src/View/chat.php
+++ src/View/chat.php
@@ -5,14 +5,14 @@
         </table>
     </div>
     <div class="card mx-auto" style="margin-top: 50px; width:300px" width="50%">
-        <form action="chat.php" method="post">
+        <form id="form-chat-add-message" action="chat.php" method="post">
             <?php if ($authenticatorService->isAuthenticated()) {?>
                     <div class="card-header">
                         <h5>Message</h5>
                     </div>
                     <div class="card-body" >
 
-                     <div type="hidden" value=<?php echo $authenticatorService->getCurrentUser()->getUsername()?> name="pseudo"/>
+                     <input type="hidden" value=<?php echo $authenticatorService->getCurrentUser()->getUsername()?> name="pseudo" id="pseudo"/>
                     <!-- <input placeholder="Message" type="text" name="message" id="message" /><br /> -->
                     <textarea class="form-control" rows="3" name="message" id="message" ></textarea>
                     <input value="Envoyer" style="color: #3398FF" class="btn float-right" type="submit">
@@ -23,5 +23,26 @@
         </form>
     </div>
 </div>
+
+<script>
+
+    $(function(){
+        $('#form-chat-add-message').submit(function(event){
+            // cancels the form submission
+            event.preventDefault();
+            $.get({
+                url:"chat.php",
+                data:{
+                    pseudo: $('#pseudo').val(),
+                    message: $('#message').val()
+                },success:function () {
+                    alert("Big boss");
+                }
+                }
+            )
+        })
+    })
+
+</script>
 
 
