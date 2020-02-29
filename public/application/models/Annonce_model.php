0a00f5b2d0a903a55c723c445ef9741cf268d0c5
Mehdi <mehdabdallaoui@gmail.com>
2020-02-29 17:00:59 +0100

    Lister les annonces selon la date de publication

--- public/application/models/Annonce_model.php
+++ public/application/models/Annonce_model.php
@@ -3,7 +3,21 @@
 class Annonce_model extends CI_Model
 {
 
-  public function insert($data){
+  public function insert(){
+
+	  /*$format = "%Y-%M-%d %H:%i";
+	  $date = "echo @mdate($format)"*/
+	  //Setting values for table columns
+
+	  //$id_user = $this->session->userdata('id_user');
+	  $data = array(
+	  	 //'id_user' => $id_user,
+		  'titre' => $this->input->post('titre'),
+		  'description' => $this->input->post('descri'),
+		  'prix' => $this->input->post('prix'),
+		  'vendu' => false,
+		  'nb_signal' => 0
+	  );
 		return $this->db->insert('annonce',$data);
   }
 
@@ -26,14 +40,24 @@ class Annonce_model extends CI_Model
   */
   public function getAllAnnonce(){
 
-	  $this->db->select('*')
-             ->from('annonce');
-      
-    return $this->db->get()->result_array();
+	  $this->db->select('*');
+	  $this->db->from("annonce");
+	  $this->db->order_by("date_publication", "ASC");
+	  return $this->db->get()->result_array();
   }
 
   public function getAnnonce($idAnnonce){
 		  return $this->db->get_where('annonce',array('id_annonce' => $idAnnonce))->result_array();
   }
+
+  public function minPrice(){
+  		$this->db->select_min('prix');
+  		return $this->db->get('annonce')->row()->prix;
+  }
+
+	public function maxPrice(){
+  		$this->db->select_max('prix');
+		return $this->db->get('annonce')->row()->prix;
+	}
 
 }
