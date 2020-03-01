5af8992fcf52d012d5d5b70995b945741a8f3e8a
Pierre Danel <pierre.g.danel@gmail.com>
2020-03-01 13:28:22 +0100

    Fixed

--- src/User/UserRepository.php
+++ src/User/UserRepository.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace User;
+
 use DateTimeInterface;
 use Exception;
 use PDO;
@@ -48,7 +50,7 @@ class UserRepository
         $stmt = $this->connection->prepare('SELECT * FROM "user" JOIN userorganization ON ("user".id=userorganization.iduser) WHERE idorganization = :idorg');
         $stmt->bindValue(':idorg', $orgId, PDO::PARAM_INT);
         $stmt->execute();
-        $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
         $users = [];
         foreach ($rows as $row) {
             $users[] = [
@@ -63,21 +65,27 @@ class UserRepository
     public function fetchByOrganizationNotInProject(int $projId)
     {
         //TODO Voir pour faire mieux
-        $stmt = $this->connection->
-            prepare('SELECT * FROM "user" 
+        $stmt = $this->connection->prepare('SELECT * FROM "user" 
                                 WHERE id NOT IN (
-                                SELECT id FROM "user" 
-                                JOIN userproject ON ("user".id = userproject.iduser) 
-                                WHERE idproject = :idproj) AND isadmin = FALSE');
+                                    SELECT id FROM "user" 
+                                    JOIN userproject ON ("user".id = userproject.iduser) 
+                                    WHERE idproject = :idproj) 
+                                AND id IN (
+                                    SELECT id from "user" 
+                                    JOIN userorganization ON ("user".id = userorganization.iduser)
+                                    WHERE idorganization = (SELECT id FROM project WHERE id = :idprojb))
+                                AND isadmin = FALSE');
+
         $stmt->bindValue(':idproj', $projId, PDO::PARAM_INT);
+        $stmt->bindValue(':idprojb', $projId, PDO::PARAM_INT);
         $stmt->execute();
-        $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
         $users = [];
         foreach ($rows as $row) {
             $users[] = [
                 "user" => $this->userHydrator->hydrateObj($row),
-                "role" => $row->role??'',
-                "date" => $row->date??''
+                "role" => $row->role ?? '',
+                "date" => $row->date ?? ''
             ];
         }
         return $users;
@@ -86,12 +94,11 @@ class UserRepository
     public function fetchByOrganizationNotInOrga()
     {
         //TODO Voir pour faire mieux
-        $stmt = $this->connection->
-            prepare('SELECT * FROM "user" 
+        $stmt = $this->connection->prepare('SELECT * FROM "user" 
                                 WHERE id NOT IN (
                                 SELECT iduser FROM userorganization) AND isadmin = FALSE ');
         $stmt->execute();
-        $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
         $users = [];
         foreach ($rows as $row) {
             $users[] = [
@@ -106,7 +113,7 @@ class UserRepository
         $stmt = $this->connection->prepare('SELECT * FROM "user" JOIN userproject ON ("user".id=userproject.iduser) WHERE idproject = :idproj');
         $stmt->bindValue(':idproj', $projectId, PDO::PARAM_INT);
         $stmt->execute();
-        $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
         $users = [];
         foreach ($rows as $row) {
             $users[] = [
@@ -123,7 +130,7 @@ class UserRepository
         $stmt = $this->connection->prepare('SELECT * FROM "user" JOIN usermeeting ON ("user".id=usermeeting.iduser) WHERE idmeeting = :idmeeting');
         $stmt->bindValue(':idmeeting', $meetingId, PDO::PARAM_INT);
         $stmt->execute();
-        $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
         $users = [];
         foreach ($rows as $row) {
             $users[] = [
@@ -146,7 +153,7 @@ class UserRepository
         $stmt->bindValue(':id', $userId, PDO::PARAM_INT);
         $stmt->execute();
         $rawUser = $stmt->fetch(PDO::FETCH_OBJ);
-        if (!$rawUser){
+        if (!$rawUser) {
             throw new Exception('Aucun utilisateur.');
         }
         return $this->userHydrator->hydrateObj($rawUser);
@@ -163,7 +170,7 @@ class UserRepository
         $stmt->bindValue(':username', $username, PDO::PARAM_STR);
         $stmt->execute();
         $rawUser = $stmt->fetch(PDO::FETCH_OBJ);
-        if (!$rawUser){
+        if (!$rawUser) {
             return null;
         }
         return $this->userHydrator->hydrateObj($rawUser);
@@ -180,7 +187,7 @@ class UserRepository
         $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
         $stmt->execute();
         $rawUser = $stmt->fetch(PDO::FETCH_OBJ);
-        if (!$rawUser){
+        if (!$rawUser) {
             return null;
         }
         return $this->userHydrator->hydrateObj($rawUser);
@@ -197,7 +204,7 @@ class UserRepository
         $stmt = $this->connection->prepare(
             'DELETE FROM "user" WHERE id = :id'
         );
-        $stmt->bindValue(':id',$userId,PDO::PARAM_INT);
+        $stmt->bindValue(':id', $userId, PDO::PARAM_INT);
         $stmt->execute();
     }
 
@@ -212,25 +219,24 @@ class UserRepository
      */
     public function insert(string $username, string $surname, string $name, string $mail, string $password, DateTimeInterface $creationdate, bool $isadmin = false)
     {
-        if($isadmin){
+        if ($isadmin) {
             $statement =  'INSERT INTO "user" (username, surname, name, mail, password, creationdate, isadmin)
                             VALUES (:username, :surname, :name, :mail, :password, :creationdate, :isadmin)';
-        }
-        else{
+        } else {
             $statement =  'INSERT INTO "user" (username, surname, name, mail, password, creationdate) 
                             VALUES (:username, :surname, :name, :mail, :password, :creationdate)';
         }
 
         $stmt = $this->connection->prepare($statement);
 
-        $stmt->bindValue(':username', $username,PDO::PARAM_STR);
-        $stmt->bindValue(':surname', $surname,PDO::PARAM_STR);
-        $stmt->bindValue(':name', $name,PDO::PARAM_STR);
-        $stmt->bindValue(':mail', $mail,PDO::PARAM_STR);
-        $stmt->bindValue(':password', $password,PDO::PARAM_STR);
-        $stmt->bindValue(':creationdate', $creationdate->format("Y-m-d H:i:s"),PDO::PARAM_STR);
-        if($isadmin)
-            $stmt->bindValue(':isadmin', $isadmin,PDO::PARAM_BOOL);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->bindValue(':surname', $surname, PDO::PARAM_STR);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+        $stmt->bindValue(':creationdate', $creationdate->format("Y-m-d H:i:s"), PDO::PARAM_STR);
+        if ($isadmin)
+            $stmt->bindValue(':isadmin', $isadmin, PDO::PARAM_BOOL);
 
         $stmt->execute();
     }
@@ -248,9 +254,8 @@ class UserRepository
     public function update(int $id, string $username, string $surname, string $name, string $mail, string $password, bool $isadmin = false)
     {
         $user = $this->findOneById($id);
-        if($user)
-        {
-            if($isadmin)
+        if ($user) {
+            if ($isadmin)
                 $statement = 'UPDATE "user" SET username = :username, surname = :surname, isadmin = :isadmin,
                                 name = :name, mail = :mail,password = :password WHERE id = :id';
             else
@@ -259,14 +264,14 @@ class UserRepository
 
             $stmt = $this->connection->prepare($statement);
 
-            $stmt->bindValue(':username', $username,PDO::PARAM_STR);
-            $stmt->bindValue(':surname', $surname,PDO::PARAM_STR);
-            $stmt->bindValue(':name', $name,PDO::PARAM_STR);
-            $stmt->bindValue(':mail', $mail,PDO::PARAM_STR);
-            $stmt->bindValue(':password', $password,PDO::PARAM_STR);
-            $stmt->bindValue(':id', $id,PDO::PARAM_INT);
-            if($isadmin)
-                $stmt->bindValue(':isadmin', $isadmin,PDO::PARAM_BOOL);
+            $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+            $stmt->bindValue(':surname', $surname, PDO::PARAM_STR);
+            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
+            $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            if ($isadmin)
+                $stmt->bindValue(':isadmin', $isadmin, PDO::PARAM_BOOL);
 
             $res = $stmt->execute();
         }
@@ -276,10 +281,16 @@ class UserRepository
     {
         /** @var User $user */
         $user = $this->findOneById($iduser);
-        if($user)
-        {
-            $this->update($user->getId(),$user->getUsername(),$user->getSurname(),
-                $user->getName(),$user->getMail(),$user->getPassword(),true);
+        if ($user) {
+            $this->update(
+                $user->getId(),
+                $user->getUsername(),
+                $user->getSurname(),
+                $user->getName(),
+                $user->getMail(),
+                $user->getPassword(),
+                true
+            );
         }
     }
 }
