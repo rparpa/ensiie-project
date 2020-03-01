62ab485edb91418f398fcac44ffcdab3b7939e90
MARWAN <mwgg@sfr.fr>
2020-03-01 17:25:10 +0100

    Invoice entity created

---
+++ src/Invoice/Invoice.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Invoice;
+
+
+use Order\Order;
+
+class Invoice
+{
+    /**
+     * @var int
+     */
+    private ?int $id = null;
+
+    /**
+     * @var Order
+     */
+    private Order $order;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder(): Order
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param Order $order
+     */
+    public function setOrder(Order $order): void
+    {
+        $this->order = $order;
+    }
+
+
+
+}
