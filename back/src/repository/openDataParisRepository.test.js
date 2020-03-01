8f2ccf338912b8834f62bd9b8e04039d363dd55d
a2lino <araujo_adelino@orange.fr>
2020-03-01 17:17:10 +0100

    getAllParkingSpotsVelos test

--- back/src/repository/openDataParisRepository.test.js
+++ back/src/repository/openDataParisRepository.test.js
@@ -43,4 +43,17 @@ describe('Get ParkingSpots', () => {
     expect(res.body[0]).toHaveProperty("_longitude");
   })
 
+  it('should get all parking spots velos', async () => {
+    const res = await request(app)
+        .get('/openDataParis/getAllParkingSpotsVelos')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body[0]).toHaveProperty("_parkingSpotId");
+    expect(res.body[15]["_parkingSpotId"]).toHaveLength(40);
+    expect(res.body[0]).toHaveProperty("_typeMob");
+    expect(res.body[0]).toHaveProperty("_latitude");
+    expect(res.body[0]).toHaveProperty("_longitude");
+  })
+
 });
