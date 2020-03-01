0255154b1b180612f07d0f0be20e61c761f332f3
Frescinel <frescinel.bart@ensiie.fr>
2020-03-01 17:52:48 +0100

    Ajout de la fonctionnalité de modification d'une annonce

--- public/application/models/Annonce_model.php
+++ public/application/models/Annonce_model.php
@@ -8,12 +8,6 @@ class Annonce_model extends CI_Model
   }
 
 
-  //La fonction update prend en paramètre un tableau contenant les valeurs remplaçantes des données actuelles
-  public function update($model){
-		$this->db->where('id_annonce', $model['id_annonce']);
-		return $this->db->update('annonce',$model);
-  }
-
   /**
   *	Fonction permettant de recupérer toutes les annonces
   *
@@ -89,6 +83,30 @@ class Annonce_model extends CI_Model
 
 		$this->db->insert('annonce', $data);
   }
+
+
+	/**
+	 * Fonction permettant de mettre à jour une annonce dans la bdd
+	 * 
+	* @param $user Id de l'utilisateur
+	* @param $titre Titre de l'annonce
+	* @param $description Description de l'annonce
+	* @param @prix Prix de l'annonce
+	 */
+	public function updateAnnonce($id_annonce,$user,$titre,$description,$prix,$etat){
+
+		$data = array(
+			'id_user' => $user,
+			'id_etat'=>$etat,
+			'titre' => $titre,
+			'description'=>$description,
+			'prix'=>$prix,
+		);
+		$this->db->where('id_annonce', $id_annonce);
+		return $this->db->update('annonce',$data);
+  }
+
+
 	public function getAnnonceByUser($id)
 	{
 		return $this->db->get_where('annonce',array('id_user' => $id))->result_array();
---
+++ public/application/views/gestion_annonce_view.php
@@ -0,0 +1,185 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+
+<!-- Container Boostrap -->
+</br>
+<div class="container">
+<?php
+		 if($this->session->flashdata('message')!=null)
+			echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.$this->session->flashdata('message').'</div>';
+		if($this->session->flashdata('error')!=null)
+			echo '<div class="alert alert-danger alert-dismissible fade-show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.$this->session->flashdata('error').'</div>';
+		if(validation_errors())
+			echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.validation_errors().'</div>';
+	?>
+
+    <?php 
+        //Variable modif à true si modification
+        $modif=false;
+        if($this->uri->ruri_string() != 'Annonce/ajouter_annonce')
+            $modif=true;
+        
+        //Ouverture du formulaire
+        //echo form_open($this->uri->uri_string(), array('id' => 'formModif', 'class' => 'form-horizontal'));
+        echo form_open('');
+    ?>
+    <div class="form-group">
+
+        <!--Structure de la rubrique description-->
+        <div class="card ">
+                <?php
+                    if(!$modif)
+                        echo '<h5 class="card-header text-white bg-success">Nouvelle annonce</h5>';
+                    else
+                        echo '<h5 class="card-header text-black bg-warning">Modifier annonce</h5>';
+                ?>
+            <div class="card-body">
+
+                <!-- //Ouverture de la balise row correspondant à la ligne titre -->
+                <div class="row">
+                <?php
+                //Si l'utilisateur créé une nouvelle annonce
+                    if(!$modif){
+                        echo '<div class="col-md">';
+                            echo form_label('Titre de l\'annonce :&nbsp;', 'titre','class="control-label"');
+                            echo form_input('titre', "", 'class="form-control"');
+                        echo '</div>';
+                    //Sinon
+                    }else{
+                        echo '<div class="col-md">';
+                        echo form_label('Titre de l\'annonce :&nbsp;', 'titre','class="control-label"');
+                        echo form_input('titre',$annonce_modif['titre'], 'class="form-control"');
+                    echo '</div>';
+                    }
+                ?>
+                <!-- Fermeture de la balise row correspondant à la ligne titre-->
+                </div>
+
+                </br>
+
+                <!-- //Ouverture de la balise row correspondant à la ligne titre -->
+                <div class="row">
+                <?php
+                //if($this->uri->ruri_string()!= 'formations/demande_ajout_formation')
+                //Si l'utilisation créé une nouvelle annonce
+                    if(!$modif){
+                        echo '<div class="col-md">';
+                            echo form_label('Description de l\'annonce :&nbsp;', 'description');
+                            echo form_textarea('description', "", 'class="form-control" style="resize: none;"');
+                        echo '</div>';
+                    }
+                    //Sinon
+                    else {
+                        echo '<div class="col-md">';
+                            echo form_label('Description de l\'annonce :&nbsp;', 'description');
+                            echo form_textarea('description', $annonce_modif['description'], 'class="form-control" style="resize: none;"');
+                        echo '</div>';
+                    }
+                ?>    
+                <!-- Fermeture de la balise row correspondant à la ligne titre-->
+                </div> 
+
+                </br>
+
+                <!-- //Ouverture de la balise row correspondant à la ligne prix -->
+                <div class="row">
+                <?php
+                //Si l'utilisateur créé une nouvelle annonce
+                    if(!$modif){
+                        echo '<div class="col-md-2 mb-3">';
+                            echo form_label('Prix de l\'objet :&nbsp;', 'prix','class="control-label"');
+                            echo form_input('prix', "", 'class="form-control"');
+                        echo '</div>';
+                    
+                    //Sinon
+                    }else{
+                        echo '<div class="col-md-2 mb-3">';
+                            echo form_label('Prix de l\'objet (en €):&nbsp;', 'prix','class="control-label"');
+                            echo form_input('prix', $annonce_modif['prix'], 'class="form-control"');
+                        echo '</div>';
+                    }
+
+                //Si l'utilisation créé une nouvelle annonce
+                    if(!$modif){
+                        echo '<div class="col-md-2 mb-3">';
+                            echo form_label('Etat de l\'objet :&nbsp;', 'etat','class="control-label"');
+                            echo form_dropdown('etat',$etats,5,'class="form-control"');
+                        echo '</div>';
+                    
+                    //Sinon
+                    }else{
+                        echo '<div class="col-md-2 mb-3">';
+                            echo form_label('Etat de l\'objet :&nbsp;', 'etat','class="control-label"');
+                            echo form_dropdown('etat',$etats,$annonce_modif['id_etat'],'class="form-control"');
+                        echo '</div>';
+                    }
+                ?>
+                <!-- Fermeture de la balise row correspondant à la ligne prix-->
+                </div>   
+
+            
+
+            <!-- //Ouverture de la balise row correspondant à l'upload de l'image -->
+                <div class="row">
+                <?php
+                //Si l'utilisation créé une nouvelle annonce
+                    if(1==1){
+                        echo '<div class="col-md ">';
+                            echo form_label('Image(s) de l\'objet :&nbsp;', 'image','class="control-label"');
+                            echo '<div id="dropzone" class="dropzone" ></div>';
+                        echo '</div>';
+                        
+                  
+                    //Sinon
+                    }else{
+                        echo '<div class="col-md">';
+                            echo '<b>Prix de l\'annonce : </b>' . "valeur" . '<br>';
+                        echo '</div>';
+                    echo '</br>';
+                    }
+                ?>
+                <!-- Fermeture de la balise row correspondant à l'upload de l'image-->
+                </div>           
+
+            <!-- Fermeture de la balise card-body de la rubrique description-->
+            </div>
+        <!--Fermeture de la balise card de la rubrique description-->
+        </div>
+
+    </div>
+    
+    </br>
+	<?php
+        if(!$modif=true)
+            echo form_submit('ajout_annonce','Ajouter',array('type'=>'button','class'=>"float-right btn btn-success"));
+        else 
+            echo form_submit('modif_annonce','Mettre à jour',array('type'=>'button','class'=>"float-right btn btn-warning"));          
+        echo form_close();
+    ?>
+
+</div>
+
+<script>
+
+    var myDropzone = new Dropzone("#dropzone", { 
+        url: "<?php echo (base_url('index.php/Annonce/fileUpload'));?>"
+    });
+
+    // Add restrictions
+    Dropzone.options.fileupload = {
+      acceptedFiles: 'image/*',
+      maxFilesize: 10 // MB
+    };
+
+/*      window.setTimeout(function() {
+        $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+          $(this).remove();
+        });
+      }, 2000);*/
+
+      $(".alert").on("close.bs.alert", function () {
+      $alertMsg.hide();
+      return false;
+});
+
+</script>
