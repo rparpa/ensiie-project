c0544220b58c4bedb266998f7c8c46ed1d23a932
Frescinel <frescinel.bart@ensiie.fr>
2020-03-01 12:02:34 +0100

    Résolution du problème d'accès au menu déroulant

--- public/application/views/annonces_view.php
+++ public/application/views/annonces_view.php
@@ -53,7 +53,6 @@
 </div>
 <!-- /.container -->
 
-
 <!-- Bootstrap core JavaScript -->
-<script src="../../assets/jquery/jquery.min.js"></script>
-<script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!--<script src="../../assets/jquery/jquery.min.js"></script>-->
+<!--<script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>-->
