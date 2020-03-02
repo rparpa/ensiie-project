0de8c86783c8ba170c2341e063e1954d7b08a5fc
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2020-03-02 23:57:11 +0100

    Added button Admin

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -3,10 +3,20 @@
     <b-nav tabs>
       <b-nav-item disabled><b>Park Ton Char Logo</b></b-nav-item>
       <UserLog class="ml-auto"/>
+      <div class="admin"><b-button variant="warning">Admin</b-button></div>
     </b-nav>
   </div>
 
 </template>
+
+<style>
+.admin{
+  display: flex;
+  align-content: center;
+  padding: 15px;
+}
+
+</style>
 
 <script>
 import UserLog from './UserLog'
