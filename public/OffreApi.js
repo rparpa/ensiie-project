f8c597f892dd91ca766644f3f73b04841caa77dd
Corentin Simon <corentin@MacBook-Pro-de-Corentin.local>
2020-03-02 08:49:03 +0100

    ajout nouvelles fonctions

--- public/OffreApi.js
+++ public/OffreApi.js
@@ -1,16 +1,33 @@
-const OffreApi = require('../src/Model/Entity/Offre');
+const OffreEntity = require('../src/Model/Entity/Offre');
 module.exports = class  {
     constructor(httpClient) {
         this.httpClient = httpClient;
     }
 
     searchOffres(titre, localisation, typeContrat, salaire, dateParution) {
+
+        var d = new Date();
+        if(dateParution==='Tout') {
+
+        }
+        else if (dateParution==='< 1 mois') {
+            d.setDate(d.getDate() - 30);
+        }
+        else if (dateParution==='< 2 semaines') {
+            d.setDate(d.getDate() - 14);
+        }
+        else if (dateParution==='< 1 semaine') {
+            d.setDate(d.getDate() - 7);
+        }
+        //alert(d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear())
+
         return this.httpClient.fetch('/searchOffres', {}).then(rows => {
             return rows.map(row => {
-                let Offre = new OffreApi();
+                let Offre = new OffreEntity();
+                Offre.id = row.id;
                 Offre.titre = row.titre;
                 Offre.description = row.description;
-                Offre.document = row.description;
+                Offre.document = row.document;
                 Offre.typeContrat = row.typeContrat;
                 Offre.adresse = row.adresse;
                 Offre.salaire = row.salaire;
