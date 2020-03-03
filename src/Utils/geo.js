58e626d425ef3496fef6a9c7a881717f18a680ca
aonoezreal <youssef.bettayeb@ensiie.fr>
2020-03-03 15:35:46 +0100

    tests geo

--- src/Utils/geo.js
+++ src/Utils/geo.js
@@ -1,10 +1,14 @@
+const fetch = require("node-fetch");
 module.export = class {
     constructor() {
     }
 
-    async getAdrnpByCoords(latitude,longitude) { // fonction async necessite async contexte lors de l'appel
+    async getAdrrByCoords(latitude,longitude) { // fonction async necessite async contexte lors de l'appel
         if(!latitude || !longitude){
             throw 'Parameter missing'
+        }
+        if(isNaN(latitude) || isNaN(longitude)){
+            throw 'Parameters must be numbers';
         }
         var url = "https://nominatim.openstreetmap.org/reverse?format=json&"
         var encodedURL = url + "lat=" + latitude + "&lon=" + longitude;
@@ -22,16 +26,23 @@ module.export = class {
         }
 
         
-    ComputeDistance(lat1,lat2,lon1,lon2){ // Formule de Haversine pour la distance à vol d'oiseau
-    var R = 6371; // km
-    var dLat = (lat2-lat1).toRad();
-    var dLon = (lon2-lon1).toRad(); 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
-            Math.sin(dLon/2) * Math.sin(dLon/2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c;
-
-    return d;
-}
+        ComputeDistance(lat1,lat2,lon1,lon2){ // Formule de Haversine pour la distance à vol d'oiseau
+        // On travaille avec des radians donc tout nos angles sont convertis en radians en multipliant par pi / 180 
+       if(!lat1 ||!lat2 || !lon1||!lon2){
+           throw 'Parameter missing';
+       }
+       if(isNaN(lat1) ||isNaN(lat2) || isNaN(lon1)|| isNaN(lon2)){
+        throw 'Parameter must be number';
+    }
+        var R = 6371; // km
+        var dLat = (lat2-lat1)*Math.PI / 180;
+        var dLon = (lon2-lon1)*Math.PI / 180; 
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(lat1*Math.PI / 180) * Math.cos(lat2*Math.PI / 180) *
+                Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c;
+    
+        return d;
+    }
 }
---
+++ src/Utils/geo.test.js
@@ -0,0 +1,145 @@
+const Offre = require('./geo');
+describe("Test of GetAddress", () => {
+
+    test('Test of Computing Distance',() => {
+        let Geo = new geo();
+        
+        function Compute(){
+            Geo.Compute(52.5487429714954,-1.81602098644987);
+        }
+        expect(getAddr).toHaveReturnedWith("137, Pilkington Avenue, Sutton Coldfield, Birmingham, West Midlands Combined Authority, West Midlands, Angleterre, B72 1LH, Royaume-Uni");
+        
+    });
+    test('Missing Lat',()=> {
+
+        let Geo = new geo();
+
+        function GetAddr(){
+            Geo.getAdrrByCoords(52.5487429714954);
+
+        }
+        expect(getAddr).toThrow(new Error('Parameter missing'));
+    })
+
+    test('Missing Lon',()=> {
+
+        let Geo = new geo();
+
+        function GetAddr(){
+            Geo.getAdrrByCoords(null,52.5487429714954);
+
+        }
+        expect(getAddr).toThrow(new Error('Parameter missing'));
+    })
+    
+    test('Lon NaN',()=> {
+
+        let Geo = new geo();
+
+        function GetAddr(){
+            Geo.getAdrrByCoords("abc",52.5487429714954);
+
+        }
+        expect(getAddr).toThrow(new Error('Parameters must be numbers'));
+    })
+    test('Lat NaN',()=> {
+
+        let Geo = new geo();
+
+        function GetAddr(){
+            Geo.getAdrrByCoords(46.213,"abc");
+        }
+        expect(getAddr).toThrow(new Error('Parameters must be numbers'));
+    })
+
+
+});
+// describe('Test of GetAddress', function () {
+
+//     test('Test Getting an actual address', () => {
+//         let Geo = new geo();
+        
+//         function Compute(){
+//             Geo.Compute(48.859315,48.859675,2.293957,2.292365);
+//         }
+//         expect(Compute).toHaveReturnedWith(0.12315179265289337);
+        
+//     });
+//     test('Missing Lat1',()=> {
+
+//         let Geo = new geo();
+//         function Compute(){
+//             Geo.Compute(null,48.859675,2.293957,2.292365);
+//         }
+//         expect(Compute).toThrow(new Error('Parameter missing'));
+//     })
+
+//     test('Missing Lon1',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.859315,null,2.293957,2.292365);
+
+//         }
+//         expect(Compute).toThrow(new Error('Parameter missing'));
+//     })
+    
+//     test('Missing lat2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.859315,48.859675,null,2.292365);
+
+//         }
+//         expect(Compute).toThrow(new Error('Parameter missing'));
+//     })
+//     test('Missing lon2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.33,48.859675,2.293957,null);
+//         }
+//         expect(Compute).toThrow(new Error('Parameter missing'));
+//     })
+//     test('Missing lon2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute("abc",45.23,2.293957,25.3);
+//         }
+//         expect(Compute).toThrow(new Error('Parameter must be number'));
+//     })
+//     test('Missing lon2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.859315,48.859675,"abc",25.3);
+//         }
+//         expect(Compute).toThrow(new Error('Parameter must be number'));
+//     })
+//     test('Missing lon2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.859315,48.859675,2.293957,"abc");
+//         }
+//         expect(Compute).toThrow(new Error('Parameter must be number'));
+//     })
+//     test('Missing lon2',()=> {
+
+//         let Geo = new geo();
+
+//         function Compute(){
+//             Geo.Compute(48.859315,"abc",2.293957,25.3);
+//         }
+//         expect(Compute).toThrow(new Error('Parameter missing'));
+//     })
+
+
+// });
