8a26083e118ce675e5cb6f0041e9a5c1f3cb50bb
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2020-03-03 01:24:33 +0100

    Added Add user button and table with mocked users

--- front/src/views/Admin.vue
+++ front/src/views/Admin.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="about">
+  <div>
     <Header/>
     <Nav class="navButtons"/>
-    <h1>This the admin page</h1>
+    <div class="userTable">
+    <div class="btn btn-success">Add User</div>  
+      <b-table striped hover :fields="fields" :items="items"  class="mt-3" >
+        <template v-slot:cell(edit)="data">
+            <div class="btn btn-warning">Edit</div>
+        </template>
+        <template v-slot:cell(delete)="data">
+            <div class="btn btn-danger">Delete</div>
+        </template>
+    </b-table>
+  </div>    
   </div>
 </template>
 
@@ -12,6 +22,12 @@
     left: 0px;
     top: 20vw;
     z-index: 99999;
+  }
+
+  .userTable{
+    padding-top: 40px; 
+    width: 70vw;
+    margin: auto;
   }
 </style>
 
@@ -25,6 +41,27 @@ export default {
   components: {
     Header,
     Nav
-  }
+  },
+   data() {
+      return {
+        fields: [
+          'age',
+
+          'first_name',
+
+          'last_name',
+          // A regular column
+          'edit',
+          // A regular column
+          'delete',
+        ],
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
 }
 </script>
