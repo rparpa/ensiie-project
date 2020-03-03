9584f993b4376494882784f41436064fd7745fba
Frescinel <frescinel.bart@ensiie.fr>
2020-03-03 14:51:15 +0100

    Ajout d'une fenetre de confirmation de suppression d'une annonce

--- public/application/views/annonceTable.php
+++ public/application/views/annonceTable.php
@@ -29,10 +29,38 @@
                 <td>".$ann['nb_signal']."</td>
                 <td>".$ann['date_publication']."</td>
                 <td>
-                    <a href=".site_url('/Annonce/supprimer_annonce/'.$ann['id_annonce'])." class=\"glyphicon glyphicon-flag\" title=\"Signaler\" data-toggle=\"tooltip\">Supprimer</i></a>
-                </td></tr>";
-			}
-			?>
+					<a href=\"#\" class=\"delete\" title=\"Bannir\" data-toggle=\"modal\" data-target=\"#suppressionModal".$ann['id_annonce']."\"><i class=\"material-icons\">&#xE5C9;</i></button>
+					</td></tr>";
+					?>
+					<!-- The Modal -->
+					<div class="modal fade" id="suppressionModal<?php echo $ann['id_annonce'];?>" >
+						<div class="modal-dialog">
+						<div class="modal-content">
+	
+							<!-- Modal Header -->
+							<div class="modal-header text-white bg-danger">
+							<h4 class="modal-title">Suppression de l'annonce <?php echo $ann['titre']?></h4>
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							</div>
+							
+							<!-- Modal body -->
+							<div class="modal-body">
+							&Ecirctes-vous sûr de vouloir supprimer cette annonce ?
+							</br>Cette action est irreversible.
+							</div>
+							
+							<!-- Modal footer -->
+							<div class="modal-footer">								
+								<button type="button" class="btn btn-danger" onclick="window.location.replace('<?php echo site_url('/Annonce/supprimer_annonce/'.$ann['id_annonce']); ?>');">Supprimer</button>
+								<button type="button" class="btn btn-secondary " data-dismiss="modal" aria-hidden="true">Annuler</button>
+							</div>
+							
+						</div>
+						</div>
+					</div>
+					<?php
+					}
+					?>
 
 			</tbody>
 		</table>
