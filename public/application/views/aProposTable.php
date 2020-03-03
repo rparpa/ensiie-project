d0338bb3121ac36adbe805f32513e0fcf43d1a0e
Mehdi <mehdabdallaoui@gmail.com>
2020-03-03 01:09:33 +0100

    Update de a_propos() + la vue

--- public/application/views/aProposTable.php
+++ public/application/views/aProposTable.php
@@ -18,7 +18,7 @@
 
 				Nous sommes de ceux qui pensent que le bonheur se partage et se transmet. Nous sommes convaincus que la proximité est génératrice d’opportunités. L’idée est toute simple, mais elle participe à changer le monde !
 
-				AnnoncIIE, avec des solutions pragmatiques, bienveillantes et efficaces, s’implique donc au quotidien pour encourager les échanges entre les IIEns, en offrant une interface homme-machine assez ergonomique facilitant la navigation et la gestion du site web dédiées aux ventes ainsi qu'aux offres de travail aux étudiants de l'ENSIIE.<br><br><div style="font-size: small">Copyright © 2020 AnnoncIIE</div></td>
+				<b>AnnoncIIE</b>, avec des solutions pragmatiques, bienveillantes et efficaces, s’implique donc au quotidien pour encourager les échanges entre les IIEns, en offrant une interface homme-machine assez ergonomique facilitant la navigation et la gestion du site web dédiées aux ventes ainsi qu'aux offres de travail aux étudiants de l'ENSIIE.<br><br><div style="font-size: small"><b>Copyright © 2020 AnnoncIIE</b></div></td>
 			</tbody>
 		</table>
 	</div>
