410c59c158e007459034ac967b57087e41e4cd34
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2020-03-04 14:53:32 +0100

    Improve user management

--- front/src/components/UserLog.vue
+++ front/src/components/UserLog.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="logvue">
-    <b-form class= "my-3" inline @submit="onLogIn" v-if="currentUser===undefined">
-      <label class="sr-only" for="login-form-username">Username</label>
-      <b-input
-        id="login-form-username"
-        class="mr-2"
-        v-model="logIn.username"
-        placeholder="Username"
-      ></b-input>
+    <b-form class= "my-3" inline @submit.stop.prevent="onLogIn" v-if="currentUser===null">
+      <b-form-group id="signin-form-group-username">
+        <b-form-input
+          id="login-form-input-username"
+          placeholder="Username"
+          class="mr-2"
+          v-model="logIn.username"
+        ></b-form-input>
+      </b-form-group>
 
       <label class="sr-only" for="login-form-password">Password</label>
       <b-input
@@ -22,8 +23,11 @@
         <!-- Using modifiers -->
         <b-button v-b-modal.signInModal @submit="onSignIn">Inscription</b-button>
         <!-- The modal -->
-        <b-modal id="signInModal" title="Inscription" hide-footer>
+        <b-modal id="signInModal" title="Inscription" @hidden="onHideSignInModal" hide-footer>
             <b-form @submit.stop.prevent="onSignIn">
+              <b-alert v-model="signInErrorAlert" variant="danger">
+                Nom d'utilisateur ou adresse email déjà utlisée.
+              </b-alert>
               <b-form-group id="signin-form-group-username">
                 <b-form-input
                   id="signin-form-input-username"
@@ -31,11 +35,11 @@
                   class="mb-2"
                   v-model="$v.signIn.username.$model"
                   :state="validateSignInState('username')"
-                  aria-describedby="input-username-live-feedback"
+                  aria-describedby="signin-username-live-feedback"
                 ></b-form-input>
 
                 <b-form-invalid-feedback
-                  id="input-username-live-feedback"
+                  id="signin-username-live-feedback"
                   >Ce champ doit comporter au moins 5 caractères.
                 </b-form-invalid-feedback>
               </b-form-group>
@@ -58,13 +62,13 @@
                 type="password"
                 v-model="$v.clearPasswords.clearSignInPasswordHolder.$model"
                 :state="validateClearPasswordState('clearSignInPasswordHolder')"
-                aria-describedby="input-password-live-feedback"
+                aria-describedby="signin-password-live-feedback"
               ></b-form-input>           
 
-            <b-form-invalid-feedback
-              id="input-password-live-feedback"
-              >Ce champ doit comporter au moins 6 caractères.
-            </b-form-invalid-feedback>
+              <b-form-invalid-feedback
+                id="signin-password-live-feedback"
+                >Ce champ doit comporter au moins 6 caractères.
+              </b-form-invalid-feedback>
             </b-form-group>
             <b-button type="submit" variant="primary">Créer un compte</b-button>
           </b-form>
@@ -82,7 +86,7 @@
       </p>
       <b-row>
         <b-button class= "mr-2" v-b-modal.settingsModel @submit="onUpdateSettings">Paramètres</b-button>
-        <b-modal id="settingsModel" title="Paramètres" hide-footer>
+        <b-modal id="settingsModel" title="Paramètres" @hidden="onHideSettingsModal" hide-footer>
             <b-form @submit="onUpdateSettings">
             <label class="sr-only" for="settings-form-username">Username</label>
             <b-input
@@ -108,9 +112,7 @@
             <b-button type="submit" variant="primary">Mettre a jour</b-button>
           </b-form>
         </b-modal>
-
-
-        <b-button variant="danger" @click="currentUser = undefined">Déconnexion</b-button>
+        <b-button variant="danger" @click="onLogOut">Déconnexion</b-button>
       </b-row>
     </div>
   </div>
@@ -150,13 +152,14 @@ export default {
         email: '',
         password: ''
       },
-      currentUser: undefined,
+      currentUser: null,
       clearPasswords: {
         clearLogInPasswordHolder: null,
         clearSignInPasswordHolder: null,
         clearSettingsPasswordHolder: null,
       },
-      displayAuthenticationErrorAlert: false
+      displayAuthenticationErrorAlert: false,
+      signInErrorAlert: false
     }
   },
   validations: {
@@ -174,21 +177,21 @@ export default {
     }
   },
   watch: {
-    clearLogInPasswordHolder: function(value) {
+    'clearPasswords.clearLogInPasswordHolder': function(value) {
       sha256(value);
       let hash = sha256.create();
-      hash.update(value);
+      hash.update(value); 
 
       this.logIn.password = hash.hex();
     },
-    clearSignInPasswordHolder: function(value) {
+    'clearPasswords.clearSignInPasswordHolder': function(value) {
       sha256(value);
       let hash = sha256.create();
       hash.update(value);
 
       this.signIn.password = hash.hex();
     },
-    clearSettingsPasswordHolder: function(value) {
+    'clearPasswords.clearSettingsPasswordHolder': function(value) {
       sha256(value);
       let hash = sha256.create();
       hash.update(value);
@@ -225,9 +228,9 @@ export default {
     },
     onSignIn(evt) {
       this.$v.signIn.$touch();
-      if (this.$v.signIn.$anyError) {
+      if (this.$v.signIn.$anyError || this.$v.clearPasswords.$anyError) {
         return;
-      }
+      } 
 
       evt.preventDefault()
       
@@ -236,9 +239,16 @@ export default {
       )
       .then(response => {
         this.currentUser = new User(response.data.username, response.data.encryptedPassword, response.data.email)
+        this.signIn.username = '';
+        this.signIn.email = '';
+        this.settings.username = this.currentUser.username
+        this.settings.email = this.currentUser.email
+        this.clearPasswords.clearSignInPasswordHolder = '';
+        this.signInErrorAlert = false;
       })
       .catch(error => {      
         console.log(error);
+        this.signInErrorAlert = true;
       })
     },
     onUpdateSettings(evt) {
@@ -253,10 +263,27 @@ export default {
 
       HTTP.post('updateUser'
       , postObject)
-      .then(this.currentUser = new User(postObject.newUsername, postObject.newPassword, postObject.newEmail))
+      .then(
+        this.currentUser = new User(postObject.newUsername, postObject.newPassword, postObject.newEmail)
+      )
       .catch(error => {      
         console.log(error);
       })
+    },
+    onLogOut(evt) {
+      this.currentUser = null;
+      this.logIn.username = '';
+      this.logIn.password = '';
+    },
+    onHideSignInModal(evt) {
+      this.signIn.username = '';
+      this.signIn.email = '';
+      this.clearPasswords.clearSignInPasswordHolder = '';
+      this.signInErrorAlert = false;
+    },
+    onHideSettingsModal(evt) {
+      this.settings.username = this.currentUser.username
+      this.settings.email = this.currentUser.email
     }
   }
 }
