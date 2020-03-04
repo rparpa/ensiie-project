395d36677c09b29e1e388d02c965b4abd0e7d9da
sbab917 <odite972@hotmail.fr>
2020-03-04 17:05:35 +0100

    Fork Pokedex

--- data/db.sql
+++ data/db.sql
@@ -1,19 +1,80 @@
-CREATE TABLE "user" (
+CREATE TABLE "user"(
     id SERIAL PRIMARY KEY ,
-    firstname VARCHAR NOT NULL ,
-    lastname VARCHAR NOT NULL ,
-    birthday date
+    login VARCHAR UNIQUE NOT NULL ,
+    mdp VARCHAR NOT NULL
 );
 
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Yvette', 'Angel', '1932-01-24');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Amelia', 'Waters', '1981-12-01');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Manuel', 'Holloway', '1979-07-25');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Alonzo', 'Erickson', '1947-11-13');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Otis', 'Roberson', '1995-01-09');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jaime', 'King', '1924-05-30');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Vicky', 'Pearson', '1982-12-12)');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '1971-03-02');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+CREATE TABLE "favorie"(
+    id_user  VARCHAR NOT NULL,
+    id_pokemon INTEGER,
+    PRIMARY KEY(id_user,id_pokemon)
+);
+
+INSERT INTO "user"(login,  mdp) VALUES ('john.doe','test');
+INSERT INTO "user"(login,  mdp) VALUES ('yvette.angel','test');
+INSERT INTO "user"(login,  mdp) VALUES ('amelia.waters','test');
+INSERT INTO "user"(login,  mdp) VALUES ('manuel.holloway','test');
+INSERT INTO "user"(login,  mdp) VALUES ('alonzo.erickson','test');
+INSERT INTO "user"(login,  mdp) VALUES ('otis.roberson','test');
+INSERT INTO "user"(login,  mdp) VALUES ('jaime.king','test');
+
+
+create table "pokemon"(
+    abilities  VARCHAR (255) ,
+    against_bug NUMERIC,
+    against_dark NUMERIC,
+    against_dragon NUMERIC,
+    against_electric NUMERIC,
+    against_fairy NUMERIC,
+    against_fight NUMERIC,
+    against_fire NUMERIC,
+    against_flying NUMERIC,
+    against_ghost NUMERIC,
+    against_grass NUMERIC,
+    against_ground NUMERIC,
+    against_ice NUMERIC,
+    against_normal NUMERIC,
+    against_poison NUMERIC,
+    against_psychic NUMERIC,
+    against_rock NUMERIC,
+    against_steel NUMERIC,
+    against_water NUMERIC,
+    attack INTEGER,
+    base_egg_steps NUMERIC,
+    base_happiness NUMERIC,
+    base_total NUMERIC,
+    capture_rate NUMERIC,
+    classfication VARCHAR (255),
+    defense INTEGER,
+    experience_growth NUMERIC,
+    height_m NUMERIC,
+    hp NUMERIC,
+    japanese_name VARCHAR (255),
+    english_name VARCHAR (255),
+    french_name VARCHAR (255),
+    percentage_male NUMERIC,
+    pokedex_number INTEGER PRIMARY KEY,
+    sp_attack INTEGER,
+    sp_defense INTEGER,
+    speed INTEGER,
+    type1 VARCHAR (255),
+    type2 VARCHAR (255),
+    weight_kg NUMERIC,
+    generation INTEGER,
+    is_legendary INTEGER
+);
+
+COPY "pokemon"(abilities ,    against_bug,    against_dark,    against_dragon,    against_electric,    against_fairy,    against_fight,    against_fire,    against_flying,    against_ghost,    against_grass,    against_ground,    against_ice,    against_normal,    against_poison,    against_psychic,    against_rock,    against_steel,    against_water,    attack,    base_egg_steps,    base_happiness,    base_total,    capture_rate,    classfication,    defense,    experience_growth,    height_m,    hp,    japanese_name,    english_name,    french_name,    percentage_male,    pokedex_number,    sp_attack,    sp_defense,    speed,    type1,    type2,    weight_kg,    generation,    is_legendary
+)
+FROM '/var/www/html/data/pokemon.csv' DELIMITER ';' CSV HEADER ENCODING 'windows-1251';
+
+/*
+admin NUMERIC DEFAULT 0
+
+INSERT INTO "user"(login, firstname, lastname, mdp,admin) VALUES ('mehdi.tahri','Mehdi', 'Tahri','test',1);
+
+CREATE TABLE "favorie" (
+    id_user INTEGER PRIMARY KEY ,
+    id_pokemon INTEGER PRIMARY KEY
+);
+*/
