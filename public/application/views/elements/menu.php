5f5e80e19817154b99190bdbc3bb0764ee62abeb
Frescinel <frescinel.bart@ensiie.fr>
2020-03-04 01:52:56 +0100

    Correction du bouton d'ajout d'une nouvelle annonce

--- public/application/views/elements/menu.php
+++ public/application/views/elements/menu.php
@@ -43,8 +43,11 @@
 			</ul>
 		</div>
 		<?php
-			if($this->session->userdata['logged_in']['droit_publication'])echo "<button type=\"button\" class=\"btn btn-success\" style=\"margin-right: 2%;\" onclick=\"window.location.replace('<?php echo site_url('/Annonce/ajouter_annonce'); ?>');\">Nouvelle annonce ...</button>";
+		 
+			if($this->session->userdata['logged_in']['droit_publication'])
 		?>
+		<button type="button" class="btn btn-success" style="margin-right: 2%;" onclick="window.location.replace('<?php echo site_url('/Annonce/ajouter_annonce'); ?>');">Nouvelle annonce ...</button>
+				
 		<div class="dropdown open">
 			<button class="btn btn-secondary dropdown-toggle"
 					type="button" id="dropdownMenu5" data-toggle="dropdown"
