9ab67a58374e91d3d7988a6185a2dd2824b37e5e
mikael <mikael.almeida@hotmail.fr>
2021-05-06 17:15:38 +0200

    WIP : log user

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,28 @@ jQuery(document).ready(function($) {
         $("#"+$(this)[0].name).val(localStorage.getItem($(this)[0].name));
     });
 });
+
+
+function check_connection(){
+    let name = $("#username").val();
+    let pwd =  $("#password").val();
+
+    alert(name + " " + pwd);
+    $.ajax({
+        url: '../src/Controller/verify_user.php',
+        type: 'POST',
+        data: {
+            username: name,
+            pwd: pwd
+        },
+        dataType: 'json',
+        success: function(data){
+            console.log('Connnected');
+            alert('success');
+        }
+    })
+    alert('FAIL');
+
+    alert('r');
+
+}
