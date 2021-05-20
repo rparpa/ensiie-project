ae40d5628ae5c5f9e88f1b48abc1e832b2922da7
mikael <mikael.almeida@hotmail.fr>
2021-05-20 13:46:15 +0200

    feat: allow log in/out

--- public/js/index.js
+++ public/js/index.js
@@ -2,37 +2,59 @@ jQuery(document).ready(function($) {
     init();
 });
 
+function connectedDisplay(){
+    $("#affiche_name").html(localStorage.getItem("username"));
+    $(".onlyUser").show();
+
+    $(".notUser").hide();
+    
+    if(localStorage.getItem('isadmin') == 'true')   $(".onlyAdmin").show();
+    else $(".onlyAdmin").hide();
+}
+
 function init(){
+    if(localStorage.getItem('connected') == 'true'){
+        connectedDisplay();
+        return;
+    }
     $('.onlyUser, .onlyAdmin, .alertField').hide();
 
     $("#username").change(function(){
         localStorage.setItem("username", $("#username").val())
     });
-
     $("#username").val(localStorage.getItem("username"));
 }
 
 
 function verify_user(){
     let name = $("#username").val();
-    let pwd =  $("#password").val();
 
     $.ajax({
         url: 'router.php',
         type: 'POST',
         data: {
-            url: "src/Controller/verify_user.php",
+            request: "Controller/verify_user.php",
             username: name,
-            pwd: pwd
+            pwd: $("#password").val()
         },
         dataType: 'json',
         success: function(data){
-            if(data.success == 1){
+            if(data.status == "success"){
                 localStorage.setItem('username', name);
-                $("#affiche_name").html(name);
-                $(".onlyUser").show();
-                $(".notUser").hide();            
+                localStorage.setItem('connected', true);
+                connectedDisplay();      
+            }
+            else{
+                // TODO show error somewhere
             }
         }
     });
+}
+
+function deconnection(){
+    localStorage.setItem('username', '');
+    localStorage.setItem('connected', null);
+    localStorage.setItem('isadmin', null);
+    
+    window.location.replace('index.html');
 }
