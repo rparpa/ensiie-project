ec4b4837f58e393e563da8f528379e0dde1bfe24
mikael <mikael.almeida@hotmail.fr>
2021-05-20 16:18:59 +0200

    feat: static functions to verify

--- src/Model/User.php
+++ src/Model/User.php
@@ -7,7 +7,6 @@ class User{
 
     private string $username;
     private string $mail;
-    private string $password;
     private bool $admin;
     
     
@@ -23,24 +22,15 @@ class User{
         return $this->admin;
     }
 
-    public static function test(){
-        echo "SALU LE MOND3";
-        return "SALUZ";
-    }
-
     public static function verifyPassword($username, $password, $pdo){
-        file_put_contents('php://stderr', print_r("HELP1 \n", TRUE));
-
         $sql = 'SELECT passwd FROM public.User WHERE username = ?';
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(1, $username);
         $stmt->execute();
         
         $rows = $stmt->rowCount();
-        file_put_contents('php://stderr', print_r($rows."\n", TRUE));
         if($rows == 1){
             $result = $stmt->fetch();
-            file_put_contents('php://stderr', print_r("I exis\n", TRUE));
             return password_verify($password, $result['passwd']);
         }
         elseif($rows > 1){
@@ -48,6 +38,37 @@ class User{
             file_put_contents('php://stderr', print_r($msg."\n", TRUE));
         }
         return false;
+    }
+
+    public static function createUser($username, $email, $password, $pdo){
+        $cryptedPwd = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $date = date('Y-m-d', time());
+        
+        $sql = 'INSERT INTO public.User (USERNAME, EMAIL, PASSWD, CREATION_DATE, VALIDATE) VALUES (?, ?, ?, ?, FALSE)';
+        
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $username); $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $cryptedPwd); $stmt->bindParam(4, $date);
+        $stmt->execute();
+
+        $result = $stmt->rowCount();
+        return $result == 1;
+    }
+
+    public static function emailExist($email, $pdo){
+        $sql = 'SELECT * FROM public.User WHERE email = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $email);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public static function checkUsername($username, $pdo){
+        $sql = 'SELECT * FROM public.User WHERE username = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $username);
+        $stmt->execute();
+        return $stmt->rowCount() == 1;
     }
 }
 ?>
