a2a85b94c632e6519e09d81d4ea69b47bc995c19
Quentin LECHASLES <quentin.lechasles@gmail.com>
2021-05-20 11:01:23 +0200

    Modification de la page d'accueil

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// IMPORT DB CONNECTION
+const connection = require('./config/database');
+
+
+// SET VIEW ENGINE
+app.set('view engine','html');
+app.engine('html', express);
+app.set('views','views');
+
+// USE BODY-PARSER MIDDLEWARE
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get('/', (req, res) => {
+  // FETCH ALL THE POSTS FROM DATABASE
+  //connection.query('SELECT * FROM posts', (err, results) => {
+    //if (err) throw err;
+    // RENDERING INDEX.HTML FILE WITH ALL POSTS
+    res.render('vue/index',{
+      //posts:results
+    });
+  //});
+
+});
+
