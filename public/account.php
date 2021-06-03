336fd1e033d686dcf2ba937ca060b9624dc412be
Leigende <59770218+Leigende@users.noreply.github.com>
2021-06-03 12:48:12 +0200

    Update account.php
    Ajout de la nouvelle navbar de wissam

--- public/account.php
+++ public/account.php
@@ -3,26 +3,7 @@
 <html lang="fr">
 <?php include("header.html"); ?>
 <body>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <div class="mdl-layout__header mdl-layout__header--waterfall">
-            <div class="mdl-layout__header-row">
-                <span>
-                    <a href="/index.html" id="title_Wikipediie">Wikiped<span style="color: #4998a7">IIE</span></a>
-                </span>
-                <div class="mdl-layout-spacer"></div>
-                <form id="connection_form">
-                    <nav class="mdl-navigation">
-                        <span id="affiche_name" class="onlyUser"></span>
-                        <input placeholder="Username" class="mdl-textfield__input connection_input notUser" type="text" id="username">
-                        <input placeholder="Password" class="mdl-textfield__input connection_input notUser" type="password" id="password">
-                    </nav>
-                </form>
-                <button style="background-color: #424242;" onclick="verify_user();" class="mdl-button mdl-js- mdl-button--raised mdl-js-ripple-effect mdl-button--accent notUser" id="connection_btn">Connexion</button>
-                <button style="background-color: #424242;" onclick="add_new_article()" class="mdl-button mdl-js- mdl-button--raised mdl-js-ripple-effect mdl-button--accent onlyUser" id="new_article_btn">Ajouter un article</button>
-                <button style="background-color: #424242;" onclick="deconnection()" class="mdl-button mdl-js- mdl-button--raised mdl-js-ripple-effect mdl-button--accent onlyUser" id="deconnection_btn">Deconnexion</button>
-            </div>
-        </div>
-
+    <?php include("navbar.html"); ?>
         <div class="mdl-layout__drawer">
             <span class="mdl-layout-title">
                 <a href="/index.html"><img class="icon-image" src="logo/logo.png" alt="logo.png"></a>
