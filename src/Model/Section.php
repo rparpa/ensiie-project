88c346deb889d481225235fa28676f8ba330ba22
mikael <mikael.almeida@hotmail.fr>
2021-06-12 22:34:48 +0200

    feat: update Section & Article

--- src/Model/Section.php
+++ src/Model/Section.php
@@ -7,12 +7,14 @@ class Section{
 
     private PDO $pdo;
 
+    private int $id;
     private string $title;
     private string $content;
     private int $pageId;
 
-    public function __construct($pdo, $pageId, $title, $content){
+    public function __construct($pdo, $id, $pageId, $title, $content){
         $this->pdo = $pdo;
+        $this->id = $id;
         $this->title = $title;
         $this->content = $content;
         $this->pageId = $pageId;
@@ -25,7 +27,16 @@ class Section{
         $stmt->bindParam(2, $this->content);
         $stmt->bindParam(3, $this->pageId);
         $stmt->execute();
+
+        $this->id = $this->pdo->lastInsertId();
     }
+
+    public function getId(){ return $this->id; }
+    public function getTitle(){ return $this->title; }
+    public function getContent(){ return $this->content; }
+    public function getPageId(){ return $this->pageId; }
+    
+
 }
 
 
